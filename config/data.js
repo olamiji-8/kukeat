a8577bd912b02f00a8e3eb8e16fa55e34f3e7bb1
olamiji-8 <olamijisotomi@gmail.com>
2023-11-12 23:16:00 +0100

    update the code

--- config/data.js
+++ config/data.js
@@ -1,326 +1,321 @@
-export default menu[
-    {
-        id:1,
-        title:"Golden Penny Noodles",
-        description:"Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
-        price:"#600",
-        category:"Pasta"
-    },
-  
-    {
-        id:2,
-        title:"Golden Penny Noodles",
-        description:"Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
-        price:"#800",
-        category:"Pasta"
-    },
- 
-    {
-        id:3,
-        tite:"Golden Penny Twist",
-        description:"Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
-        price:"#600"
-        category:"Pasta"
-    },
-  
-    {
-        id:4,
-        title:"Golden Penny Noodles",
-        description:"Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
-        price:"#900",
-        category:"Pasta"
-    },
- 
-    {
-        id:5,
-        title:"Golden Penny Noodles",
-        deascription:"Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
-        price:"#600"
-        category:"Pasta"
-    },
-   
-    
+export default [
   {
-    id:6,
-    title:"Golden Penny Twist",
-    description:"Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
-    price:"#900"
-    category:"Pasta"
+    id: 1,
+    title: "Golden Penny Noodles",
+    description: "Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
+    price: "#600",
+    category: "Pasta",
   },
-    
+
   {
-    id:7,
-    title:"Golden Penny Noodles",
-    description:"Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
-    price:"#600",
-    category:"Pasta"
+    id: 2,
+    title: "Golden Penny Noodles",
+    description: "Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
+    price: "#800",
+    category: "Pasta",
   },
- 
-    
-{
-    id:8,
-    title:"Golden Penny Noodles",
-    description:"Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
-    price:"#800",
-    category:"Pasta"
-},
-    
-    
-{
-    id:9,
-    tite:"Golden Penny Twist",
-    description:"Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
-    price:"#600"
-    category:"Groceries"
-},
-    
-    
-{
-    id:10,
-    title:"Golden Penny Noodles",
-    description:"Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
-    price:"#800",
-    category:"Groceries"
-},
-    
-{
-    id:11,
-    title:"Golden Penny Noodles",
-    deascription:"Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
-    price:"#900"
-    category:"Groceries"
-    
-},
-   {
-    id:12,
-    title:"Golden Penny Twist",
-    description:"Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
-    price:"#600"
-    category:"Groceries"
-   },
-    
-   {
-    id:13,
-    title:"Golden Penny Twist",
-    description:"Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
-    price:"#900"
-    category:"Groceries"
-   },
-    
- {
-    id:14,
-    title:"Golden Penny Twist",
-    description:"Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
-    price:"#800"
-    category:"Groceries"
-    
- },
-    {
-        id:15,
-    title:"Golden Penny Twist",
-    description:"Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
-    price:"#600"
-    category:"Groceries"
-    },
-    
-   {
-    id:16,
-    title:"Golden Penny Twist",
-    description:"Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
-    price:"#900"
-    category:"Groceries"
-   },
-    
-   {
-    id:17,
-    title:"Golden Penny Twist",
-    description:"Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
-    price:"#600"
-    category:"Beverages"
-   },
-    
+
   {
-    id:18,
-    title:"Golden Penny Twist",
-    description:"Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
-    price:"#900"
-    category:"Beverages"
+    id: 3,
+    title: "Golden Penny Twist",
+    description: "Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
+    price: "#600",
+    category: "Pasta",
   },
-    
- {
-    id:19,
-    title:"Golden Penny Twist",
-    description:"Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
-    price:"#600"
-    category:"Beverages"
- },
-    
+
   {
-    id:20,
-    title:"Golden Penny Twist",
-    description:"Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
-    price:"#800"
-    category:"Beverages"
+    id: 4,
+    title: "Golden Penny Noodles",
+    description: "Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
+    price: "#900",
+    category: "Pasta",
   },
-    {
-        
-    id:21,
-    title:"Golden Penny Twist",
-    description:"Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
-    price:"#900"
-    category:"Beverages"
-    },
-    
+
   {
-    id:22,
-    title:"Golden Penny Twist",
-    description:"Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
-    price:"#800"
-    category:"Beverages"
-    
+    id: 5,
+    title: "Golden Penny Noodles",
+    description: "Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
+    price: "#600",
+    category: "Pasta",
   },
-  
+
   {
-    id:23,
-    title:"Golden Penny Twist",
-    description:"Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
-    price:"#600"
-    category:"Beverages"
-    
+    id: 6,
+    title: "Golden Penny Twist",
+    description: "Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
+    price: "#900",
+    category: "Pasta",
   },
+
   {
-    id:24,
-    title:"Golden Penny Twist",
-    description:"Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
-    price:"#900"
-    category:"Beverages"
+    id: 7,
+    title: "Golden Penny Noodles",
+    description: "Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
+    price: "#600",
+    category: "Pasta",
   },
-    
-   {
-    id:25,
-    title:"Golden Penny Twist",
-    description:"Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
-    price:"#600"
-    category:"Swallow"
-   },
-    
- {
-    id:26,
-    title:"Golden Penny Twist",
-    description:"Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
-    price:"#800"
-    category:"Swallow"
- },
-    
+
   {
-    id:27,
-    title:"Golden Penny Twist",
-    description:"Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
-    price:"#900"
-    category:"Swallow"
+    id: 8,
+    title: "Golden Penny Noodles",
+    description: "Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
+    price: "#800",
+    category: "Pasta",
   },
-    
+
   {
-    id:28,
-    title:"Golden Penny Twist",
-    description:"Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
-    price:"#600"
-    category:"Swallow"
+    id: 9,
+    title: "Golden Penny Twist",
+    description: "Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
+    price: "#600",
+    category: "Groceries",
   },
-    
-{
-    id:29,
-    title:"Golden Penny Twist",
-    description:"Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
-    price:"#900"
-    category:"Swallow"
-},
-    
+
   {
-    id:30,
-    title:"Golden Penny Twist",
-    description:"Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
-    price:"#800"
-    category:"Swallow"
-    
+    id: 10,
+    title: "Golden Penny Noodles",
+    description: "Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
+    price: "#800",
+    category: "Groceries",
   },
+
   {
-    id:31,
-    title:"Golden Penny Twist",
-    description:"Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
-    price:"#600"
-    category:"Swallow"
-    
+    id: 11,
+    title: "Golden Penny Noodles",
+    description: "Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
+    price: "#900",
+    category: "Groceries",
   },
-    {
-        id:32,
-    title:"Golden Penny Twist",
-    description:"Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
-    price:"#900"
-    category:"Swallow"
-    },
-    
-   {
-    id:33,
-    title:"Golden Penny Twist",
-    description:"Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
-    price:"#600"
-    category:"Oil"
-   },
-    
-   {
-    id:34,
-    title:"Golden Penny Twist",
-    description:"Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
-    price:"#800"
-    category:"Oil"
-   },
-    
-   {
-    id:35,
-    title:"Golden Penny Twist",
-    description:"Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
-    price:"#900"
-    category:"Oil"
-   },
-    
+
   {
-    id:36,
-    title:"Golden Penny Twist",
-    description:"Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
-    price:"#800"
-    category:"Oil"
+    id: 12,
+    title: "Golden Penny Twist",
+    description: "Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
+    price: "#600",
+    category: "Groceries",
   },
-    
-   {
-    id:37,
-    title:"Golden Penny Twist",
-    description:"Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
-    price:"#600"
-    category:"Oil"
-    
-   },
+
   {
-    id:38,
-    title:"Golden Penny Twist",
-    description:"Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
-    price:"#800"
-    category:"Oil"
+    id: 13,
+    title: "Golden Penny Twist",
+    description: "Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
+    price: "#900",
+    category: "Groceries",
   },
-    
-   {
-    id:39,
-    title:"Golden Penny Twist",
-    description:"Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
-    price:"#900"
-    category:"Oil"
-   },
-    
-    {
-        id:40,
-    title:"Golden Penny Twist",
-    description:"Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
-    price:"#800"
-    category:"Oil"
-    }
-]
+
+  {
+    id: 14,
+    title: "Golden Penny Twist",
+    description: "Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
+    price: "#800",
+    category: "Groceries",
+  },
+
+  {
+    id: 15,
+    title: "Golden Penny Twist",
+    description: "Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
+    price: "#600",
+    category: "Groceries",
+  },
+
+  {
+    id: 16,
+    title: "Golden Penny Twist",
+    description: "Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
+    price: "#900",
+    category: "Groceries",
+  },
+
+  {
+    id: 17,
+    title: "Golden Penny Twist",
+    description: "Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
+    price: "#600",
+    category: "Beverages",
+  },
+
+  {
+    id: 18,
+    title: "Golden Penny Twist",
+    description: "Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
+    price: "#900",
+    category: "Beverages",
+  },
+
+  {
+    id: 19,
+    title: "Golden Penny Twist",
+    description: "Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
+    price: "#600",
+    category: "Beverages",
+  },
+
+  {
+    id: 20,
+    title: "Golden Penny Twist",
+    description: "Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
+    price: "#800",
+    category: "Beverages",
+  },
+
+  {
+    id: 21,
+    title: "Golden Penny Twist",
+    description: "Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
+    price: "#900",
+    category: "Beverages",
+  },
+
+  {
+    id: 22,
+    title: "Golden Penny Twist",
+    description: "Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
+    price: "#800",
+    category: "Beverages",
+  },
+
+  {
+    id: 23,
+    title: "Golden Penny Twist",
+    description: "Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
+    price: "#600",
+    category: "Beverages",
+  },
+
+  {
+    id: 24,
+    title: "Golden Penny Twist",
+    description: "Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
+    price: "#900",
+    category: "Beverages",
+  },
+
+  {
+    id: 25,
+    title: "Golden Penny Twist",
+    description: "Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
+    price: "#600",
+    category: "Swallow",
+  },
+
+  {
+    id: 26,
+    title: "Golden Penny Twist",
+    description: "Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
+    price: "#800",
+    category: "Swallow",
+  },
+
+  {
+    id: 27,
+    title: "Golden Penny Twist",
+    description: "Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
+    price: "#900",
+    category: "Swallow",
+  },
+
+  {
+    id: 28,
+    title: "Golden Penny Twist",
+    description: "Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
+    price: "#600",
+    category: "Swallow",
+  },
+
+  {
+    id: 29,
+    title: "Golden Penny Twist",
+    description: "Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
+    price: "#900",
+    category: "Swallow",
+  },
+
+  {
+    id: 30,
+    title: "Golden Penny Twist",
+    description: "Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
+    price: "#800",
+    category: "Swallow",
+  },
+
+  {
+    id: 31,
+    title: "Golden Penny Twist",
+    description: "Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
+    price: "#600",
+    category: "Swallow",
+  },
+
+  {
+    id: 32,
+    title: "Golden Penny Twist",
+    description: "Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
+    price: "#900",
+    category: "Swallow",
+  },
+
+  {
+    id: 33,
+    title: "Golden Penny Twist",
+    description: "Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
+    price: "#600",
+    category: "Oil",
+  },
+
+  {
+    id: 34,
+    title: "Golden Penny Twist",
+    description: "Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
+    price: "#800",
+    category: "Oil",
+  },
+
+  {
+    id: 35,
+    title: "Golden Penny Twist",
+    description: "Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
+    price: "#900",
+    category: "Oil",
+  },
+
+  {
+    id: 36,
+    title: "Golden Penny Twist",
+    description: "Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
+    price: "#800",
+    category: "Oil",
+  },
+
+  {
+    id: 37,
+    title: "Golden Penny Twist",
+    description: "Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
+    price: "#600",
+    category: "Oil",
+  },
+
+  {
+    id: 38,
+    title: "Golden Penny Twist",
+    description: "Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
+    price: "#800",
+    category: "Oil",
+  },
+
+  {
+    id: 39,
+    title: "Golden Penny Twist",
+    description: "Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
+    price: "#900",
+    category: "Oil",
+  },
+
+  {
+    id: 40,
+    title: "Golden Penny Twist",
+    description: "Lorem ipsum dolor sit amet tetur mattis vel dhc kd.",
+    price: "#800",
+    category: "Oil",
+  },
+];
