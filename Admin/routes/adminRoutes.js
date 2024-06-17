f123eb5c2c6babca8f6912d02f70bb0662436449
olamiji-8 <olamijisotomi@gmail.com>
2024-06-18 00:21:45 +0100

    update the code

--- Admin/routes/adminRoutes.js
+++ Admin/routes/adminRoutes.js
@@ -1,17 +1,11 @@
 // adminRoutes.js
 const express = require('express');
 const router = express.Router();
-const itemController = require('../controller/itemController');
 const cartItemController = require('../controller/cartItemController');
 
 
 
-// // Item management
-// router.get('/items', itemController.getAllItems);
-// router.post('/items', itemController.createItem);
-// router.get('/items/:itemId', itemController.getItem);
-// router.put('/items/:itemId', itemController.updateItem);
-// router.delete('/items/:itemId', itemController.deleteItem);
+
 
 // Cart item management
 router.get('/cart-items', cartItemController.getAllCartItems);
